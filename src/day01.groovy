f131851c752f0919517942649254dee7450fb293
Balázs Zaicsek <zebalu@gmail.com>
2025-01-04 12:21:00 +0100

    compact

--- src/day01.groovy
+++ src/day01.groovy
@@ -1,15 +1,9 @@
 def input = new File("../input-01.txt").text
 def left = [], right = []
-input.lines().map { it.split($/ {3}/$) }.forEach {
-    left << (it[0] as int)
-    right << (it[1] as int)
-}
+input.lines().map { it.split($/ {3}/$) }.forEach {[left << (it[0] as int), right << (it[1] as int)]}
 
-left.sort()
-right.sort()
-
-def part1 = [left, right].transpose().collect { Math.abs(it[0] - it[1]) }.sum()
+def part1 = [left.sort(), right.sort()].transpose().collect { Math.abs(it[0] - it[1]) }.sum()
 println part1
 
-def part2 = left.collect( lhs -> lhs * right.count{lhs==it}).sum()
+def part2 = left.collect(lhs -> lhs * right.count { lhs == it }).sum()
 println part2
