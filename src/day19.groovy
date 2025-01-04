a56e0481da3b25c31653d6bb73d91ff0e1e4d096
Balázs Zaicsek <zebalu@gmail.com>
2025-01-04 13:06:48 +0100

    format

--- src/day19.groovy
+++ src/day19.groovy
@@ -4,7 +4,11 @@ def towels = parts[0].split(', ').toList()
 def designs = parts[1].split('\n').toList()
 
 def countWays
-countWays = { design -> towels.findAll { design.startsWith(it) }.collect { it == design ? 1L : countWays(design.substring(it.size()))/*, towels)*/ }.inject(0L, Long::sum) }.memoize()
+countWays = { design ->
+    towels.findAll { design.startsWith(it) }.collect {
+        it == design ? 1L : countWays(design.substring(it.size()))
+    }.inject(0L, Long::sum)
+}.memoize()
 
 println designs.count { countWays(it) > 0 }
 println designs.collect { countWays(it) }.sum()
