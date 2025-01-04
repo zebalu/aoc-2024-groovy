fba4e1e73d6ba9dcd468d7fae3f71a730467b38a
Balázs Zaicsek <zebalu@gmail.com>
2025-01-04 12:41:42 +0100

    compact

--- src/day11.groovy
+++ src/day11.groovy
@@ -3,19 +3,14 @@ def numbers = input.split(" ").collect { it as long }
 
 def isEvenLength = { n -> Long.toString(n).size() % 2 == 0 }.memoize()
 
-def split = { n ->
-    def str = Long.toString(n)
-    [str.substring(0, str.size().intdiv(2)) as long, str.substring(str.size().intdiv(2)) as long]
-}.memoize()
+def split = { n, str=Long.toString(n) -> [str.substring(0, str.size().intdiv(2)) as long, str.substring(str.size().intdiv(2)) as long]}.memoize()
 
 def size
 size = { stone, steps ->
     if (steps == 0) 1L
     else if (stone == 0) size(1, steps - 1)
-    else if (isEvenLength(stone)) {
-        def (begin, end) = split(stone)
-        size(begin, steps - 1) + size(end, steps - 1)
-    } else size(stone * 2024L, steps - 1)
+    else if (isEvenLength(stone)) size(split(stone).first, steps - 1) + size(split(stone).last, steps - 1)
+    else size(stone * 2024L, steps - 1)
 }.memoize()
 
 println numbers.collect { size(it, 25) }.sum()
